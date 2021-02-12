5f7a931d9fc77699d604eff0c9d9353984b48f5f
LiuLin <242585237@qq.com>
2021-02-12 20:31:35 +0800

    作业代码

--- src/Zuoye.java
+++ src/Zuoye.java
@@ -164,4 +164,100 @@ public class Zuoye {
     }
 }
      */
+
+       /* String str=new String("hello");
+        char[]ch={'a','b'};
+        public static void main(String args[]){
+            Zuoye ex=new Zuoye();
+            ex.change(ex.str,ex.ch);
+            System.out.print(ex.str+" and ");
+            System.out.print(ex.ch);
+        }
+        public void change(String str,char ch[]){
+            str="test ok"; ch[0]='c';
+        }
+        //hello and cb
+        */
+           /*private float f = 1.0f;
+           int m = 12;
+           static int n = 1;
+       public static void main (String args[]) {
+           Zuoye t = new Zuoye();
+
+       } }
+            */
+    /*
+    给定一个整数数组，判断是否存在重复元素
+    class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null||nums.length<2){
+            return false;
+        }
+        Set<Integer> map=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.contains(nums[i])){
+                return true;
+            }
+            map.add(nums[i]);
+        }
+        return false;
+    }
 }
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null||nums.length<2){
+            return false;
+        }
+        Arrays.sort(nums);
+        int j=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[j]){
+                return true;
+            }
+            j++;
+        }
+        return false;
+    }
+}
+/*
+你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
+
+你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True
+
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        if(name==null||typed==null){
+            return false;
+        }
+        int i=name.length();
+        int j=typed.length();
+        if(i>j){
+            return false;
+        }
+        i=0;
+        j=0;
+        while(i<name.length()&&j<typed.length()){
+            if(name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }else if(j>0&&typed.charAt(j)==typed.charAt(j-1)){
+                j++;
+            }else{
+                return false;
+            }
+        }
+        while(j<typed.length()){
+            if(typed.charAt(j)!=name.charAt(name.length()-1)){
+                return false;
+            }
+            j++;
+        }
+        return i==name.length();
+
+    }
+}
+     */
+
+}
+
+
